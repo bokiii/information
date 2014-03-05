7d47aaac4e2295a21c35f2de522bc7083e72af66
Rey Sanchez <rey.sanchez05@gmail.com>
2014-03-05 14:22:28 +0800

    some tests

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,7 +7,7 @@ class Home extends CI_Controller {
 	}
 	
 	function index() {
-		echo "<p>WVSU-JC Online Student Information System is under construction.</p>";
+		echo "<p>Wvsu-Jc Online Student Information System is under construction.</p>";
 	}
 	
 	
