b3b54ef55335a75301d93474d8203a2270c70a53
Rey Sanchez <rey.sanchez05@gmail.com>
2014-03-05 13:38:58 +0800

    fixed the configs

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://information.cloudcontrolled.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'patanoy@*05';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/controllers/home.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+	}
+	
+	function index() {
+		echo "<p>WVSU-JC Online Student Information System is under construction.</p>";
+	}
+	
+	
+}
